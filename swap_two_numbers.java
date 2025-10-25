ebe3d558caaa2c7274f61c0adc408f0d71176746
vivekds19 <dsvivek092@gmail.com>
2025-10-25 19:29:12 +0530

    without using temp variable

---
+++ swap_two_numbers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class swap_two_numbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number (a):");
+        int a = scanner.nextInt();
+
+        System.out.println("Enter the second number (b):");
+        int b = scanner.nextInt();
+
+        System.out.println("Before swapping: a = " + a + ", b = " + b);
+
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("After swapping: a = " + a + ", b = " + b);
+
+       
+    }
+}
