bd60f0aa4d9ea1d59f4c43e16ecf50db6db945ad
vivekds19 <dsvivek092@gmail.com>
2025-10-26 09:09:24 +0530

    last digit of the number

---
+++ last_digit.java
@@ -0,0 +1,8 @@
+public class last_digit {
+    public static void main(String[] args) {
+        int number = 12345;
+        int lastDigit = number % 10;
+        System.out.println("The last digit of " + number + " is: " + lastDigit);
+    }
+    
+}
