59dab986974f3d02f54d53498da6d684c279f831
vivekds19 <dsvivek092@gmail.com>
2025-10-27 11:00:37 +0530

    wrapper class

---
+++ wrapper.java
@@ -0,0 +1,14 @@
+public class wrapper{
+  public static void main(String[] args) {
+      Integer x1=400,x2=400; //for small values it gives same for large values it crate different object for the variables 
+
+      if(x1==x2)
+      {
+        System.out.println("same");
+      }
+      else{
+        System.out.println("not same");
+      }
+      
+  }
+}
