f93c9cb8188a3b2c093c8a1b293f4d9b3ea9441a
vivekds19 <dsvivek092@gmail.com>
2025-10-27 10:55:17 +0530

    Autoboxing and AutoUnboxing

---
+++ wrapper_class.java
@@ -0,0 +1,10 @@
+public class wrapper_class {
+  public static void main(String[] args) {
+      int x1=10;
+      Integer x2=x1;//AutoBoxing
+      int x3=x2; // AutoUnboxing
+      System.out.println(x1);
+      System.out.println(x2);
+      System.out.println(x3); 
+  }
+}
