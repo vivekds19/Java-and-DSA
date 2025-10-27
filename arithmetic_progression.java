c65e3f8047e8b5feda9b78d0fc867520c21944ea
vivekds19 <dsvivek092@gmail.com>
2025-10-27 11:08:27 +0530

    arithmetic_progression

--- arithmetic_progression.java
+++ arithmetic_progression.java
@@ -2,24 +2,22 @@
 import java.util.Scanner;
 
 // find the nth term value 
-
 // a - starting point  d- is the differnece b/w them n-find the value 
-public class arithmetic_progression{
-public static int n_term(int a,int d,int n)
-{
-  int result=a+(n-1)*d;
-  return result;
-}
+public class arithmetic_progression {
 
-public static void main (String [] args)
-{
-  Scanner sc=new Scanner(System.in);
-  System.out.println("Enter the value of a:");
-  int a=sc.nextInt();
-  System.out.println("Enter the value of d:");
-  int d=sc.nextInt();
-  System.out.println("Enter the value of n:");
-  int n=sc.nextInt();
-  System.out.println(n_term(a,d,n));
-}
+    public static void  n_term(int a, int d, int n) {
+        int result = a + (n - 1) * d;
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of a:");
+        int a = sc.nextInt();
+        System.out.println("Enter the value of d:");
+        int d = sc.nextInt();
+        System.out.println("Enter the value of n:");
+        int n = sc.nextInt();
+        n_term(a, d, n);
+    }
 }
