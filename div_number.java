d83ea4cf4a97587e6cfb739e8740d1d0aebc7ce8
vivekds19 <dsvivek092@gmail.com>
2025-10-28 11:41:06 +0530

    All divison of  numbers

---
+++ div_number.java
@@ -0,0 +1,14 @@
+
+public class div_number {
+
+    public static void main(String[] args) {
+        int a = 15;
+        for (int i = 1; i <= a; i++) {
+            if (a % i == 0) {
+                System.out.println(i);
+
+            }
+        }
+    }
+
+}
