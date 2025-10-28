31c3bf086d40d89db6d1583d9e5ae412e266431a
vivekds19 <dsvivek092@gmail.com>
2025-10-28 10:37:30 +0530

    using for loop

---
+++ tables.java
@@ -0,0 +1,11 @@
+
+class tables {
+
+    public static void main(String[] args) {
+        int num = 32;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " X " + i + " = " + num * i);
+        }
+    }
+
+}
