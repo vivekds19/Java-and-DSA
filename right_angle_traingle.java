4b7c87f026687be69ec6b9365ea699e6c9f0be17
vivekds19 <dsvivek092@gmail.com>
2025-10-28 10:46:08 +0530

    using nested for loop

---
+++ right_angle_traingle.java
@@ -0,0 +1,13 @@
+
+class right_angle_traingle {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
