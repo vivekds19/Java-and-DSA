41997b93289a5a7830a661b9cd2e37b2fe71b2cc
vivekds19 <dsvivek092@gmail.com>
2025-11-06 10:15:15 +0530

    print the address of the array

---
+++ array_4.java
@@ -0,0 +1,7 @@
+public class array_4 {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30};
+
+        System.out.println(arr);  // prints something like [I@15db9742
+    }
+}
