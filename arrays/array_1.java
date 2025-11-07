c67b568e38577f43bbe48f5a9a0000cc3b61e752
vivekds19 <dsvivek092@gmail.com>
2025-11-07 09:32:11 +0530

    for each

---
+++ arrays/array_1.java
@@ -0,0 +1,10 @@
+
+class array_1 {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+}
