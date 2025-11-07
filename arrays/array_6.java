5cc2b720e56a24f88e9788c2f98d48c7f9d167ce
vivekds19 <dsvivek092@gmail.com>
2025-11-07 09:46:31 +0530

    sort the array at the specific index

---
+++ arrays/array_6.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+
+class java_6 {
+
+    public static void main(String[] args) {
+        int arr[] = {10, 20, 40, 50, 30};
+
+        Arrays.sort(arr, 2, 5);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
