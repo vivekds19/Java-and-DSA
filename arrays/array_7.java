877b0f554e6ec38802d0e5e0e20cba95bc42ed6b
vivekds19 <dsvivek092@gmail.com>
2025-11-07 09:50:01 +0530

    using binarysearch method

---
+++ arrays/array_7.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+
+class array_7 {
+
+    public static void main(String[] args) {
+        int arr[] = {10, 20,30, 50};
+
+        System.out.println("searching");
+        System.out.println(Arrays.binarySearch(arr, 50));
+
+    }
+}
