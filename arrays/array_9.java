21dee0ff3ffdc3c87a9f5e8f8c16ba05ae9134e8
vivekds19 <dsvivek092@gmail.com>
2025-11-07 10:02:57 +0530

    using deepEquals check for the nested array

---
+++ arrays/array_9.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+
+class array_9 {
+
+    public static void main(String[] args) {
+        int arr[][] = {{10, 20, 0},{ 40, 50}};
+        int arr2[][] = {{10, 20, 30}, {40, 50}};
+        System.out.println(Arrays.deepEquals(arr, arr2));
+    }
+
+}
