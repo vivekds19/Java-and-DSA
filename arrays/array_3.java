036065cffde055229c26dd20221bbfdc2814b2b6
vivekds19 <dsvivek092@gmail.com>
2025-11-07 09:32:30 +0530

    print array using toString

---
+++ arrays/array_3.java
@@ -0,0 +1,10 @@
+
+import java.util.Arrays;
+
+class array_3 {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(arr));
+    }
+}
