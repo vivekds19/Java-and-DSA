5ded1dd241ce370cb0e4b1631d92f02f7b5e0569
vivekds19 <dsvivek092@gmail.com>
2025-11-10 10:24:29 +0530

    using trim() method

---
+++ strings/string_14.java
@@ -0,0 +1,10 @@
+
+class string_14 {
+
+    public static void main(String[] args) {
+        String str = "       welcome     ";
+        System.out.println(str);
+        System.out.println(str.trim());
+
+    }
+}
