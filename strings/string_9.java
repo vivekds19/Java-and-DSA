2645ce8bd9440a8d6c82b3c9e189a89775797b61
vivekds19 <dsvivek092@gmail.com>
2025-11-10 10:12:41 +0530

    using subString() method

---
+++ strings/string_9.java
@@ -0,0 +1,9 @@
+class string_9 {
+
+    public static void main(String[] args) {
+        String str = "welcome";
+        System.out.println(str.substring(3,7));
+        System.out.println(str.substring(1,3));
+        System.out.println(str.substring(1));
+    }
+}
