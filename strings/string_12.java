1a810b018550a7ae4a65e19def6767982686fe7d
vivekds19 <dsvivek092@gmail.com>
2025-11-10 10:18:55 +0530

    using replace() method

---
+++ strings/string_12.java
@@ -0,0 +1,9 @@
+
+class string_12 {
+
+    public static void main(String[] args) {
+        String str = "welcome";
+        System.out.println(str.replace("welcome", "good"));
+        System.out.println(str);
+    }
+}
