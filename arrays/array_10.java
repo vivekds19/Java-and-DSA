262813e3a87799714bb2c46e9280f2576989b396
vivekds19 <dsvivek092@gmail.com>
2025-11-10 09:29:36 +0530

    using for each method

---
+++ arrays/array_10.java
@@ -0,0 +1,10 @@
+class array_10{
+  public static void main(String atrgs[])
+{
+        int[] numbers = {10, 20, 30, 40, 50};
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+    }
+}
+
