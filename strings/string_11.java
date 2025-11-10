9f6312a8fe0b87fc45e2ecb7516fc86b62e78e48
vivekds19 <dsvivek092@gmail.com>
2025-11-10 10:15:51 +0530

    using concat() method

---
+++ strings/string_11.java
@@ -0,0 +1,10 @@
+
+class string_11 {
+
+    public static void main(String[] args) {
+        String str = "welcome";
+        System.out.println(str.concat("bye"));
+        System.out.println(str);
+
+    }
+}
