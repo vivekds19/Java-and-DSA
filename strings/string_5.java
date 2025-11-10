d289c1a4c286b7c2bbc9a601634f4d1d19691ff6
vivekds19 <dsvivek092@gmail.com>
2025-11-10 10:01:15 +0530

    using toLowerCase()  and toUpperCase() method

---
+++ strings/string_5.java
@@ -0,0 +1,9 @@
+
+class string_5 {
+
+    public static void main(String[] args) {
+        String str = "Welcome";
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+    }
+}
