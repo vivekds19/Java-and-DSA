87d35fa357c647e203dc4ae8395a7e9d0709526a
vivekds19 <dsvivek092@gmail.com>
2025-11-10 09:56:44 +0530

    using indexof and lastIndexof methods

---
+++ strings/string_3.java
@@ -0,0 +1,9 @@
+
+class string_3 {
+
+    public static void main(String[] args) {
+        String str = "welcome";
+        System.out.println(str.indexOf('e'));
+        System.out.println(str.lastIndexOf('e'));
+    }
+}
