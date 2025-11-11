b268c31cfcd865de1e1f4b995f2e171e672a1667
vivekds19 <dsvivek092@gmail.com>
2025-11-11 09:33:05 +0530

    using append() method

---
+++ strings/String_Buffer/append.java
@@ -0,0 +1,7 @@
+class append{
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Hello");
+        sb.append(" World");
+        System.out.println(sb);
+    }
+}
