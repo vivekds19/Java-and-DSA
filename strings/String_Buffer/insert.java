db433d534e17e9e75fce740544adb798a7e80611
vivekds19 <dsvivek092@gmail.com>
2025-11-11 09:35:41 +0530

    using insert() method

---
+++ strings/String_Buffer/insert.java
@@ -0,0 +1,9 @@
+
+class insert {
+
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("welcome");
+        sb.insert(sb.length(), " to  java programming");
+        System.out.println(sb);
+    }
+}
