5b7560ef067e73106c3ab8c8de1a6d7b952ec31e
vivekds19 <dsvivek092@gmail.com>
2025-11-11 09:39:30 +0530

    using delete() method

---
+++ strings/String_Buffer/delete.java
@@ -0,0 +1,7 @@
+class delete{
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Hello World");
+        sb.delete(3,6);
+        System.out.println(sb);
+    }
+}
