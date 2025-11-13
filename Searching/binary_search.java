84eff52cd74fdeb081e0211a4c40007310229500
vivekds19 <dsvivek092@gmail.com>
2025-11-13 16:34:58 +0530

    Binary Search

---
+++ Searching/binary_search.java
@@ -0,0 +1,33 @@
+class binary_search{
+    static int binarySearch(int arr[], int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {10, 20, 30, 40, 50, 60, 70, 80, 90};
+        int target = 30;
+
+        int result = binarySearch(arr, target);
+
+        if (result == -1) {
+            System.out.println("Element not found");
+        } else {
+            System.out.println("Element found at index: " + result);
+        }
+    }
+
+}
