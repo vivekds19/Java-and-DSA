65516152fa059a6443cd2535549a06884058ba8b
vivekds19 <dsvivek092@gmail.com>
2025-11-18 12:47:01 +0530

    Missing Number

---
+++ DSA/Missing_numbers.java
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/missing-number/
+
+class Missing_numbers {
+
+    public static void main(String[] args) {
+        int arr[] = {3, 0, 1, 4};
+        System.out.println(missingNumber(arr));
+    }
+
+    public static int missingNumber(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i];
+            if (nums[i] < nums.length && nums[i] != nums[correct]) {
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
+            } else {
+                i++;
+            }
+
+        }
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != index) {
+                return index;
+            }
+        }
+        return nums.length;
+    }
+}
