f7368b7a4854c7e4f0fb461de58244ad5280acca
vivekds19 <dsvivek092@gmail.com>
2025-11-19 19:05:38 +0530

    print 1 to 5

---
+++ Recusion/recursion_1.java
@@ -0,0 +1,18 @@
+
+class recursion_1 {
+
+    public static void main(String[] args) {
+        number(1);
+    }
+
+    static void number(int n) {
+
+        if (n == 5) {
+            System.out.println(5);
+            return;
+        }
+        System.out.println(n);
+        number(n + 1);
+
+    }
+}
