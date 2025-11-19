2580e418a9713053b3f073002d54dc701a3b025b
vivekds19 <dsvivek092@gmail.com>
2025-11-19 12:37:14 +0530

    Inverted Right-Angled Triangle Pattern

---
+++ Patterns/patterns_5.java
@@ -0,0 +1,17 @@
+public class patterns_5 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int row = n; row >= 1; row--) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
