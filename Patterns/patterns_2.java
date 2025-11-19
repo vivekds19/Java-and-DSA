c63cb54cba97d52808c1d1ce15b7430934f8a3b9
vivekds19 <dsvivek092@gmail.com>
2025-11-19 12:08:05 +0530

    Square pattern

---
+++ Patterns/patterns_2.java
@@ -0,0 +1,15 @@
+
+class patterns_2 {
+
+    public static void main(String[] args) {
+        int n = 4;
+
+        for (int row = 0; row <= n; row++) {
+            for (int col = 0; col <= n; col++) {
+                System.out.print("* ");
+            }
+            System.err.println();
+        }
+
+    }
+}
