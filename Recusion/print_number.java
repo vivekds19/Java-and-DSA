2a3e15a7fbe4d35ab2f65c3f8f34d0c86e692a00
vivekds19 <dsvivek092@gmail.com>
2025-11-26 11:12:24 +0530

    print n from 1

---
+++ Recusion/print_number.java
@@ -0,0 +1,15 @@
+
+class print_number {
+
+    public static void main(String args[]) {
+        number(5);
+    }
+
+    static void number(int n) {
+        if (n == 0) {
+            return;
+        }
+        System.out.println(n);
+        number(n - 1);
+    }
+}
