b9765701e7cf581f08fd284c757c251572652b4b
vivekds19 <dsvivek092@gmail.com>
2025-12-02 16:16:37 +0530

    remove the character from the string

--- Recusion/Remove.java
+++ Recusion/Remove.java
@@ -2,20 +2,19 @@
 class Remove {
 
     public static void main(String[] ardss) {
-        skip("", "baccad");
+        System.out.println(skip("bccada"));
     }
 
-    static void skip(String p, String up) {
+    static String skip(String up) {
         if (up.isEmpty()) {
-            System.out.println(p);
-            return;
+            return "";
         }
         char ch = up.charAt(0);
 
         if (ch == 'a') {
-            skip(p, up.substring(1));
+            return skip(up.substring(1));
         } else {
-            skip(p + ch, up.substring(1));
+            return ch + skip(up.substring(1));
         }
     }
 }
