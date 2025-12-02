3d28e2d394a60bcfeba1b533cbe1623132d477bd
vivekds19 <dsvivek092@gmail.com>
2025-12-02 16:12:31 +0530

    remove the character from the string

---
+++ Recusion/Remove.java
@@ -0,0 +1,21 @@
+
+class Remove {
+
+    public static void main(String[] ardss) {
+        skip("", "baccad");
+    }
+
+    static void skip(String p, String up) {
+        if (up.isEmpty()) {
+            System.out.println(p);
+            return;
+        }
+        char ch = up.charAt(0);
+
+        if (ch == 'a') {
+            skip(p, up.substring(1));
+        } else {
+            skip(p + ch, up.substring(1));
+        }
+    }
+}
