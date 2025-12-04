26cb9ba462ef6cd434b74cfcad9fe8c80b5d3a26
vivekds19 <dsvivek092@gmail.com>
2025-12-04 18:27:06 +0530

    Constructor OverLoading

--- Oops/constructor_3.java
+++ Oops/constructor_3.java
@@ -25,7 +25,9 @@ public class constructor_3 {
     public static void main(String[] args) {
         Point p1 = new Point();
         Point p2 = new Point(10, 20);
+        Point p3 = p2; /*  p2 is a reference pointer that points to the object in the memory 
+        p3 will now point to the same object that p2 is pointing to.*/
         p1.print();
-        p2.print();
+        p3.print(); 
     }
 }
