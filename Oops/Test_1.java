2ee7587ecb53e6831c5f1bf3d63d5f2af57eb913
vivekds19 <dsvivek092@gmail.com>
2025-12-05 16:55:49 +0530

    Static Method

---
+++ Oops/Test_1.java
@@ -0,0 +1,12 @@
+class Demo {
+
+    static void greet() {
+        System.out.println("Hello!");
+    }
+}
+
+public class Test_1 {
+    public static void main(String[] args) {
+        Demo.greet(); // No object needed
+    }
+}
