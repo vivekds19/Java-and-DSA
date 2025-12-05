d6402c7fab7a3f31e80c8aa086aebaa60eecbed4
vivekds19 <dsvivek092@gmail.com>
2025-12-05 17:04:31 +0530

    Static Block

---
+++ Oops/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    static {
+        System.out.println("Static block executed");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Main method executed");
+    }
+}
