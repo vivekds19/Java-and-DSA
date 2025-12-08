2868f55621a7430322b39ad7166f1f24041e6470
vivekds19 <dsvivek092@gmail.com>
2025-12-08 20:24:15 +0530

    Private  access modifier

---
+++ Oops/Access_modifier/private_modifier/A.java
@@ -0,0 +1,13 @@
+
+public class A {
+
+    private int data;
+
+    public void getter(int data) {
+        this.data = data;
+    }
+
+    public int setter() {
+        return data;
+    }
+}
---
+++ Oops/Access_modifier/private_modifier/B.java
@@ -0,0 +1,10 @@
+
+public class B {
+
+    public static void main(String[] args) {
+        A a = new A();
+        a.getter(10);
+        System.out.println(a.setter());
+    }
+
+}
