15972ed5ec279b850f6393759cce3f82c7ec7367
vivekds19 <dsvivek092@gmail.com>
2025-12-08 20:30:50 +0530

    Protected access modifier

---
+++ Oops/Access_modifier/Protected_modifier/A.java
@@ -0,0 +1,7 @@
+
+public class A {
+
+    protected void show() {
+        System.out.println("Hello from protected method of A");
+    }
+}
---
+++ Oops/Access_modifier/Protected_modifier/B.java
@@ -0,0 +1,8 @@
+
+public class B extends A {
+
+    public static void main(String[] args) {
+        B obj = new B();
+        obj.show();
+    }
+}
