38a653c0a2cf67dec2b88e1361196cbaf1eb3f6e
vivekds19 <dsvivek092@gmail.com>
2025-12-09 11:26:27 +0530

    Abstract class inside a constructor

---
+++ Oops/Abstraction/Circle.java
@@ -0,0 +1,9 @@
+
+public class Circle extends Shape {
+
+    @Override
+    void draw() {
+        System.out.println("Circle is drawn");
+
+    }
+}
---
+++ Oops/Abstraction/Shape.java
@@ -0,0 +1,9 @@
+
+public abstract class Shape {
+
+    Shape() {
+        System.out.println("Shape is created");
+    }
+
+    abstract void draw();
+}
