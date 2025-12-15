83f9923e9a77d3ab57b7b138c7d1f52427355007
vivekds19 <dsvivek092@gmail.com>
2025-12-15 09:27:43 +0530

    ArrayLsit in Collections

---
+++ Collections/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+
+        list.add("A");
+        list.add("B");
+        list.add("A"); // duplicates allowed
+
+        System.out.println(list); 
+    }
+}
