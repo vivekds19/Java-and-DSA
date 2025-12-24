85c21c650a90c4ce08697ce5fbfc19f5aaff5512
vivekds19 <dsvivek092@gmail.com>
2025-12-24 13:28:35 +0530

    @Deprecated

--- Oops/Annotations.java
+++ Oops/Annotations.java
@@ -1,18 +1,37 @@
+
+class OldLibrary {
+    @Deprecated
+    public void oldMethod() {
+        System.out.println("This is the old method.");
+    }
+
+    public void newMethod() {
+        System.out.println("This is the new method.");
+    }
+}
+
 class Parent {
-  @Deprecated
+
+    @Deprecated
     void show() {
         System.out.println("Parent");
     }
 }
 
 class Child extends Parent {
+
     @Override
     void show() {
         System.out.println("Child");
     }
 }
+
 public class Annotations {
+
     public static void main(String[] args) {
+        OldLibrary lib = new OldLibrary();
+       // lib.oldMethod();// not recommended to use this method 
+        lib.newMethod();
         Parent obj = new Parent();
         obj.show();
     }
