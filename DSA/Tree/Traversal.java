07e87afb0db0499c419fc84a77209b865a69a2a4
vivekds19 <dsvivek092@gmail.com>
2025-12-25 06:40:37 +0530

    InOrder

---
+++ DSA/Tree/Traversal.java
@@ -0,0 +1,33 @@
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
+
+ class Traversal {
+    Node root;
+
+    void inorder(Node node) {
+        if (node == null) return;
+        inorder(node.left);
+        System.out.print(node.data + " ");
+        inorder(node.right);
+    }
+
+    public static void main(String[] args) {
+        Traversal tree = new Traversal();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left=new Node(4);
+        tree.root.left.right=new Node(5);
+        tree.root.right.left=new Node(6);
+        tree.root.right.right=new Node(7);
+
+        tree.inorder(tree.root); 
+    }
+}
