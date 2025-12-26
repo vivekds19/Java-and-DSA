71b81bc674b04942df231523e09183ea77912a9f
vivekds19 <dsvivek092@gmail.com>
2025-12-26 18:41:07 +0530

    HashSet

---
+++ Collections/Set/HashSetClass.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class HashSetClass {
+    public static void main(String[] args) {
+      Set<Integer> set=new HashSet<>();
+      set.add(10);
+      set.add(20);
+      set.add(30);
+      set.add(10);
+      set.add(20);
+
+      System.out.println(set);
+    }
+}
