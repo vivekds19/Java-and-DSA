6d6881ac4b9a58df2f6639de6dc1ce04a5dc2771
vivekds19 <dsvivek092@gmail.com>
2025-12-26 18:44:21 +0530

    TreeSet

---
+++ Collections/Set/TreeSetExample.java
@@ -0,0 +1,15 @@
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TreeSetExample {
+      public static void main(String[] args) {
+         Set<Integer> set=new TreeSet<>();
+      set.add(40);
+      set.add(20);
+      set.add(30);
+      set.add(50);
+      set.add(80);
+
+      System.out.println(set);
+      }
+}
