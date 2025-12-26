ed250ad6ca2f26ccf9a01a1f154e286fcb9407b1
vivekds19 <dsvivek092@gmail.com>
2025-12-26 14:12:21 +0530

    join()

---
+++ mulithread/Synchronized/Demo.java
@@ -0,0 +1,37 @@
+class Counter{
+  int count;
+  public void increment()
+  {
+    count++;
+  }
+}
+public class Demo {
+      public static void main(String[] args) throws InterruptedException {
+        Counter c=new Counter();
+
+        Runnable obj1=()->  //Lamda Expression
+          {
+              for(int i=0;i<50000;i++)
+              {
+                c.increment();
+              }
+          };
+          Runnable obj2=()->
+          {
+              for(int i=0;i<50000;i++)
+              {
+                  c.increment();
+              }
+          };
+          Thread t1=new Thread(obj1);
+          Thread t2=new Thread(obj2);
+          
+          t1.start();
+          t2.start();
+          
+          t1.join();
+          t2.join();
+        
+          System.out.println(c.count);
+      }
+}
